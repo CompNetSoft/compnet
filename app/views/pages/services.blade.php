abea836c22411f18e43c0f0308efce533c694edf
Issayev Adilet <issayev.adilet@gmail.com>
2015-02-28 16:44:07 +0600

    modal

--- app/views/pages/services.blade.php
+++ app/views/pages/services.blade.php
@@ -8,7 +8,7 @@
   <div class="container page-services">
     <br>
     <h2 class="text-center text-uppercase"><a href="{{ route('index') }}"><i class="fa fa-home"></i></a> / Услуги</h2>
-    <h4><a href="#" data-toggle="modal" data-target="#myModal"><i class="fa fa-calculator"></i> Калькулятор</a></h4>
+    <h4><a href="#" data-toggle="modal" data-target="#calculator"><i class="fa fa-calculator"></i> Калькулятор</a></h4>
     <!-- <button type="button" class="btn btn-primary" data-toggle="modal" data-target=".bs-example-modal-lg">Large modal</button> -->
     <div class="row">
       <ul class="nav nav-pills">
@@ -36,24 +36,19 @@
     </div>
   </div>
 
-  <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+  <div class="modal fade" id="calculator" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-lg">
       <div class="modal-content">
         <div class="modal-header">
           <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span class="text-muted" aria-hidden="true">&times;</span></button>
-          <h4 class="modal-title" id="myModalLabel">Modal title</h4>
+          <h4 class="modal-title" id="myModalLabel">Калькулятор</h4>
         </div>
         <div class="modal-body">
-          ...Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-          cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-          proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          ...
         </div>
         <div class="modal-footer">
-          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-          <button type="button" class="btn btn-primary">Save changes</button>
+          <button type="button" class="btn btn-default" data-dismiss="modal">Закрыть</button>
+          <button type="button" class="btn btn-primary">Отправить заявку</button>
         </div>
       </div>
     </div>
